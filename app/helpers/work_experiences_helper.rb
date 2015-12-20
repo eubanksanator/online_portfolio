f955481b66cb092cbcca29686c67622c1317c06d
Travis Eubanks <tmeubanks113@gmail.com>
2015-12-19 18:49:06 -0600

    Adds styles across the board

--- app/helpers/work_experiences_helper.rb
+++ app/helpers/work_experiences_helper.rb
@@ -1,2 +1,22 @@
 module WorkExperiencesHelper
+   @colors = ["purple", "red", "orange", "peach", "yellow", "green"]
+   # def card_color
+   #    @colors = ["purple", "red", "orange", "peach", "yellow", "green"]
+   #    @colors.each do |c|
+   #       case c
+   #       when "purple"
+   #          "purple"
+   #       when "red"
+   #          "red"
+   #       when "orange"
+   #          "orange"
+   #       when "peach"
+   #          "peach"
+   #       when "yellow"
+   #          "yellow"
+   #       when "green"
+   #          "green"
+   #       end
+   #    end
+   # end
 end
