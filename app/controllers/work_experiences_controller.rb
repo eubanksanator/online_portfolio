c4079d252a0b32babc9cef33fdcfdec60d134871
Travis Eubanks <tmeubanks113@gmail.com>
2015-12-31 12:29:47 -0600

    1

--- app/controllers/work_experiences_controller.rb
+++ app/controllers/work_experiences_controller.rb
@@ -6,7 +6,7 @@ class WorkExperiencesController < ApplicationController
   # GET /work_experiences.json
   def index
     @work_experiences = WorkExperience.all
-    @work_experiences.order(current_job: :desc)
+    @work_experiences = @work_experiences.order(current_job: :desc)
   end
 
   # GET /work_experiences/1
